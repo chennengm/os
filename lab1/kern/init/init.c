f794da9373c7d9f8dc292fc3ea6e2d81f30f46d1
annjsgh <2483779035@qq.com>
2024-09-27 19:07:32 +0800

    2213393

--- lab1/kern/init/init.c
+++ lab1/kern/init/init.c
@@ -32,6 +32,9 @@ int kern_init(void) {
     clock_init();  // init clock interrupt
 
     intr_enable();  // enable irq interrupt
+
+    asm volatile("mret");
+    asm volatile("ebreak");
     
     while (1)
         ;
